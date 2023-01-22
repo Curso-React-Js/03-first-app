6e7642e9f2cb83cd6d079b2e7140011be15266f7
Luis Angel <angelfgdeveloper@gmail.com>
2023-01-22 17:05:36 -0600

    Primer Hola Mundo en React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Para renderizar
 
+// Un componente
+const App = () => {
+  return (<h1>Hola Mundo!!!</h1>);
+}
+
+// Forma de renderizar el componente en el DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
