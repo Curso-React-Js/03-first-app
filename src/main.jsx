9d10349098d71d91b15d576c4b17874ec04e77a3
Luis Angel <angelfgdeveloper@gmail.com>
2023-01-24 10:15:43 -0600

    Primer componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Para renderizar
+import { HelloWorldApp } from './HelloWorldApp';
 
 // Un componente
-const App = () => {
-  return (<h1>Hola Mundo!!!</h1>);
-}
+// const App = () => {
+//   return (<h1>Hola Mundo!!!</h1>);
+// }
 
 // Forma de renderizar el componente en el DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelloWorldApp />
   </React.StrictMode>
 );
