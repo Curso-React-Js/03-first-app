8a84378a57ecdf5500bec526a16e92b884b4fec0
Luis Angel <angelfgdeveloper@gmail.com>
2023-02-02 20:31:00 -0600

    Primeros pasos en pruebas

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,9 +1,26 @@
 // Ejecutar test: npm run test
 
-test('Esta prueba no debe de fallar', () => {
+// Ayuda al intellicents
+// npm install -D @types/jest
 
-  if( 0 === 1 ) {
-    throw new Error('No puede dividir entre cero');
-  }
+describe('Pruebas en <DemonComponent />', () => { 
+  
+  test('Esta prueba no debe de fallar', () => {
+  
+    // if( 0 === 1 ) {
+    //   throw new Error('No puede dividir entre cero');
+    // }
+  
+    // 1. Inicializacion
+    const message1 = 'Hola Mundo';
+  
+    // 2. Estímulo
+    const message2 = message1.trim();
+  
+    // 3. Observar el comportamiento... esperado
+    // expect( message1 ).toBe( message2 );
+    expect( message1 ).toBe( message2 );
+  
+  });
 
 });
